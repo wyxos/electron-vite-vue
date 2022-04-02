b0bfb2c5a86d318e5a47632170f10560c882b2e1
Joey J <joey.jaulimsing@live.com>
2022-04-03 00:52:06 +0400

    feat: automatic version update

--- release.js
+++ release.js
@@ -1,7 +1,37 @@
-const { execSync } = require('child_process')
+import {execSync} from "child_process";
 
-require('dotenv').config()
+(async function(){
+    const { execSync } = require('child_process')
+    const prompt = require('prompt')
 
-const token = process.env.GH_TOKEN
+    require('dotenv').config()
 
-execSync(`cross-env GH_TOKEN=${token} electron-builder -- -p always`)
+    const token = process.env.GH_TOKEN
+
+    prompt.start()
+
+    const json = require('./package.json')
+
+    const currentVersion = json.version
+
+    const { version } = await prompt.get({
+        properties: {
+            version: {
+                description: `Enter the version to publish (current ${currentVersion}`,
+                before: function(value) { return 'v' + value; }
+            }
+        }
+    })
+
+    json.version = version
+
+    fs.writeFileSync('./package.json', JSON.stringify(json))
+
+    execSync('git add .')
+    console.log('Files staged.')
+
+    execSync(`git commit -m "feat: release ${version}"`)
+    console.log('Files committed.')
+
+    execSync(`cross-env GH_TOKEN=${token} electron-builder -- -p always`, {stdio: 'inherit'})
+}())
