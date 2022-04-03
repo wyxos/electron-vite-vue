22a8c649b83231d253f476e2811228b5b833a204
Joey J <joey.jaulimsing@live.com>
2022-04-03 21:06:23 +0400

    fix: parsing version into release message

--- release.js
+++ release.js
@@ -1,38 +1,41 @@
-(async function release(){
-    const { execSync } = require('child_process')
-    const prompt = require('prompt')
-    const fs = require('fs')
+(async function release() {
+  const { execSync } = require('child_process');
+  const prompt = require('prompt');
+  const fs = require('fs');
 
-    require('dotenv').config()
+  require('dotenv')
+    .config();
 
-    const token = process.env.GH_TOKEN
+  const token = process.env.GH_TOKEN;
 
-    prompt.start()
+  prompt.start();
 
-    const json = require('./package.json')
+  const json = require('./package.json');
 
-    const currentVersion = json.version
+  const currentVersion = json.version;
 
-    const { version } = await prompt.get({
-        properties: {
-            version: {
-                description: `Enter the version to publish (current ${currentVersion}`,
-                before: function(value) { return 'v' + value; }
-            }
-        }
-    })
+  const { version } = await prompt.get({
+    properties: {
+      version: {
+        description: `Enter the version to publish (current ${currentVersion}`,
+        before(value) {
+          return `v${value}`;
+        },
+      },
+    },
+  });
 
-    json.version = version
+  json.version = version;
 
-    fs.writeFileSync('./package.json', JSON.stringify(json, null, 2))
+  fs.writeFileSync('./package.json', JSON.stringify(json, null, 2));
 
-    execSync('git add .')
-    console.log('Files staged.')
+  execSync('git add .');
+  console.log('Files staged.');
 
-    const message = "feat: release ${version}";
+  const message = `feat: release ${version}`;
 
-    execSync(`git commit -m ${message}`)
-    console.log(`Commit with message: ${message}`)
+  execSync(`git commit -m ${message}`);
+  console.log(`Commit with message: ${message}`);
 
-    execSync(`cross-env GH_TOKEN=${token} electron-builder -- -p always`, {stdio: 'inherit'})
-}())
+  execSync(`cross-env GH_TOKEN=${token} electron-builder -- -p always`, { stdio: 'inherit' });
+}());
